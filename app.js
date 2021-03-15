2d037a596d89cef08120602d5837c21ab12cbd8c
Irwin <irwinestradatorres@gmail.com>
2021-03-15 12:09:55 -0500

    Create And List Task

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 // require('colors');
 
 // const { mostrarMenu, pausa } = require('./helpers/mensajes');
-const { inquirerMenu, pausa } = require('./helpers/inquirer');
+const { inquirerMenu, pausa, leerInput } = require('./helpers/inquirer');
+// const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
 
-console.clear();
+// console.clear();
 
 const main = async() => {
     console.log('Hola Mundo');
 
     let opt = '';
+    const tareas = new Tareas();
 
     do {
         opt = await inquirerMenu();
-        console.log({ opt });
+        // console.log({ opt });
+
+        // const tareas = new Tareas();
+        // const tarea = new Tarea('Comprar comida');
+
+        // tareas._listado[tarea.id] = tarea;
+
+        // console.log(tareas);
+
+        switch (opt) {
+            case '1':
+                const desc = await leerInput('Descripción:');
+                // console.log(desc);
+                tareas.crearTarea(desc);
+                break;
+            case '2':
+                console.log(tareas._listado);
+                break;
+        }
 
         await pausa();
         // if (opt !== '0')
